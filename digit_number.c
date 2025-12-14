b5542db371bb652657771e201698a57fb79fd9b6
Ai Tsujikawa <tuji.kamome@gmail.com>
2025-12-14 19:14:11 +0900

    rewrite to add digit_number between 6 to 16

--- digit_number.c
+++ digit_number.c
@@ -3,6 +3,15 @@
 int main(void)
 {
     int n = 1234;
-    printf("%d\n", n % 10);
-    printf("%d\n", n / 10);
+    int position = 1;
+
+    while (n > 0)
+    {
+        int digit = n % 10;
+        printf("position %d: digit = %d\n", position, digit);
+
+        n /= 10;
+        position++;
+    }
+    return 0;
 }
