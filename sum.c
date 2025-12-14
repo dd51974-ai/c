ac720d80319c0db445e050b0e20ee924d4910632
Ai Tsujikawa <tuji.kamome@gmail.com>
2025-12-14 22:36:30 +0900

    create to sum.c, rewrite digit number between 5 to 40

--- sum.c
+++ sum.c
@@ -2,27 +2,20 @@
 
 int main(void)
 {
-    long original_number;
-    printf("Number: ");
-    scanf("%ld", &original_number);
-
-    long number = original_number;
-
-    int position = 1;
     int sum = 0;
-
+    int position = 1;
+    int number = 1;
     while (number > 0)
-    {
-        int digit = number % 10;
-        
-        if (position % 2 == 0)
+    {   
+        int digit = number % 10;/* code */
+        if (position % 2 ==1)
         {
             sum += digit;
         }
-
         else
         {
             int doubled = digit * 2;
+
             if (doubled >= 10)
             {
                 sum += (doubled / 10) + (doubled % 10);
@@ -32,11 +25,8 @@ int main(void)
                 sum += doubled;
             }
         }
-
         number /= 10;
         position++;
     }
-
-    printf("SUM = %d\n", sum);
-    return 0;
+    
 }
