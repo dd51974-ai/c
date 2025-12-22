f14df392f0029095022ee9ee03b6d4d42cf5fbd1
Ai Tsujikawa <tuji.kamome@gmail.com>
2025-12-22 21:27:07 +0900

    add code for between 8 to 14 of hello.c

--- hello.c
+++ hello.c
@@ -5,9 +5,11 @@ int main(void)
     char name[] = "AI";
     printf("Hello %s\n", name);
 
-    long what_name;
+    char your_name[50];
     printf("What's your name?: ");
-    what_name = getchar();
-    printf("%ld\n", what_name);
-    return 0;
+    scanf("%49s\n", your_name);
+    for (int your_name = 0; your_name <=3; your_name++)
+    {
+        printf("Hello %d\n", your_name);
+    }    
 }
