e07b419f8bf39ff29d3bfd22de978a1abc8cabc2
Ai Tsujikawa <tuji.kamome@gmail.com>
2025-12-27 10:53:39 +0900

    add to write on hi.c

---
+++ hi.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char s[] = "HI!";
+    printf("%c%c%c\n", s[0], s[1], s[2]); //%s is character or strings,
+    printf("%i %i %i %i \n", s[0], s[1], s[2], s[3]); // %i is character
+}
