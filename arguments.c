30260e31d22cdcd3f1ddc79d4e4c5790bace1f74
Ai Tsujikawa <tuji.kamome@gmail.com>
2026-01-17 15:56:05 +0900

    creat arguments, length of C programming

---
+++ arguments.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int add(int a, int b) // a and b are PARAMENTERS
+{
+    return a + b;
+}
+
+int main(void)
+{
+    int result = add(3, 5); // 3 and 5 are AREGUMENTS
+}
---
+++ length.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char name[] = "Hello";
+    int length = strlen(name);
+
+    printf("%d\n", length); //5
+}
