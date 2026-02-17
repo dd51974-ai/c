43b9bc1054896855aa6bacdf82fb6515afaf222c
Ai Tsujikawa <tuji.kamome@gmail.com>
2026-02-18 06:05:10 +0900

    create code null.c and cat.c of C programming

---
+++ cat.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    word = "CAT";
+    for (int i = 0; i < strlen(word); i++)
+    {
+        char c = word[i];
+    }
+    return 0;
+}
---
+++ null.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("\\0 is %d\n", '\0');
+    printf("0 is %d\n", '0');
+    return 0;
+}
