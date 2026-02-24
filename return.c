f8f9ec3ad99deaa1b51bbe70d08d0b643800e82b
Ai Tsujikawa <tuji.kamome@gmail.com>
2026-02-24 19:34:05 +0900

    create rerurn.c code

---
+++ return.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void)
+{
+    for (int i = 0; i < 5 ; i++)
+    {
+        printf("Hello, world\n");
+    }
+
+    printf("Hello");
+    return 0;
+    printf("World");
+}
